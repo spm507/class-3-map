4f851caea308cbb780dca08bc06908308698d2a3
spm507 <57920656+spm507@users.noreply.github.com>
2020-02-20 00:27:10 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ var initOptions = {
   container: 'map-container',
   style: 'mapbox://styles/mapbox/dark-v10', // use this basemap
   center: initialCenterPoint,
-  zoom: initialZoom,
+  zoom: 11.48,
 }
 
 
